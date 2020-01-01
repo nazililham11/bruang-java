b389b324cdf355f58b5be519f3f1d6a166938db6
nazililham11 <46530014+nazililham11@users.noreply.github.com>
2020-01-01 14:24:20 +0700

    Update Main Activity

--- app/src/main/java/com/bruang/bookingruang/View/MainActivity.java
+++ app/src/main/java/com/bruang/bookingruang/View/MainActivity.java
@@ -1,8 +1,11 @@
-package com.bruang.bookingruang;
+package com.bruang.bookingruang.View;
+
+import android.content.Intent;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.bruang.bookingruang.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, LoginActivity.class));
+
     }
-}
+
+    }
